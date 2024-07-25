f172364315bf7d332d9906a65fe204a678dbde95
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-07-24 21:36:32 -0400

    Long day

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photo, user] = results;
