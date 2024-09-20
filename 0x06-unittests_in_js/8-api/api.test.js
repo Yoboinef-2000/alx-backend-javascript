6c7c9b5b195aab4ac9f5bd702dbd0b98c6368bc1
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-20 04:57:47 -0400

    Basic Integration Testing.

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,20 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('should return the correct status code', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  // Additional tests can be added here
+});
