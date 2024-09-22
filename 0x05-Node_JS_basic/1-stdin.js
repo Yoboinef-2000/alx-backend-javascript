e484f91f03785f8038eecf75ac7a5991a51606ba
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-21 21:03:51 -0400

    Using process stdin.

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data => {
-  console.log(`Your name is: ${data.toString().trim()}`);
-  console.log('This important software is now closing');
-  process.exit();
-}));
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
